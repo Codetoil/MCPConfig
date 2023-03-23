112f6de2f8904b506af4aad488c299aea46ad7dd
Curle <curle@gemwire.uk>
2023-03-23 00:09:29 +0000

    Reintroduce modern MCInjector files into the project side-by-side with legacy.

--- buildSrc/src/main/groovy/net/minecraftforge/mcpconfig/tasks/MCInjectTask.groovy
+++ buildSrc/src/main/groovy/net/minecraftforge/mcpconfig/tasks/MCInjectTask.groovy
@@ -4,8 +4,14 @@ import org.gradle.api.tasks.*
 
 //Deprecated.. Only here until we migrate all the old versions to not use MCInject
 public class MCInjectTask extends ToolJarExec {
-    @InputFile File map
-    @InputFile File exceptor
+    // We can have only one set, so all of these need to be optional...
+    @Optional @InputFile File map
+    @Optional @InputFile File exceptor
+
+    @Optional @InputFile File access
+    @Optional @InputFile File constructors
+    @Optional @InputFile File exceptions
+
     @InputFile File input
     @OutputFile File log
     @OutputFile File dest
@@ -13,8 +19,14 @@ public class MCInjectTask extends ToolJarExec {
     @Override
     protected void preExec() {
         setArgs(Utils.fillVariables(args, [
+            // MCInjector 3.7+
+            'access': access,
+            'constructors': constructors,
+            'exceptions': exceptions,
+            // MCInjector 3.6<
             'map': map,
             'exceptor': exceptor,
+            // All
             'log': log,
             'input': input,
             'output': dest
